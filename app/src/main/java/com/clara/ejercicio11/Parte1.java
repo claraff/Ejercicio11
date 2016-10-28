96a181a4332c43dfcb15254592b482e20caf1c53
Clara <clara_fer2@hotmail.com>
2016-10-28 18:00:20 +0200

    Initial commit

---
+++ app/src/main/java/com/clara/ejercicio11/Parte1.java
@@ -0,0 +1,13 @@
+package com.clara.ejercicio11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Parte1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parte1);
+    }
+}
